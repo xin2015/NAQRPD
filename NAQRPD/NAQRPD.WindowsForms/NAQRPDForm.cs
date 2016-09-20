e862f6c3754d265cc42650f086dff705a8e3128f
liuhx <564030525@qq.com>
2016-09-20 16:08:46 +0800

    winform

--- NAQRPD/NAQRPD.WindowsForms/NAQRPDForm.cs
+++ NAQRPD/NAQRPD.WindowsForms/NAQRPDForm.cs
@@ -15,54 +15,91 @@ namespace NAQRPD.WindowsForms
     public partial class NAQRPDForm : Form
     {
         static ILog logger = LogManager.GetLogger<NAQRPDForm>();
+        static DateTime beginTime;
+        static DateTime endTime;
 
         public NAQRPDForm()
         {
             InitializeComponent();
         }
 
+        private bool GetTime()
+        {
+            bool result= DateTime.TryParse(textBox1.Text, out beginTime) && DateTime.TryParse(textBox2.Text, out endTime);
+            if (!result) textBox3.Text = "获取时间失败";
+            return result;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            DateTime beginTime = new DateTime(2016, 1, 1);
-            DateTime endTime = new DateTime(2016, 9, 20, 12, 0, 0);
             try
             {
-                List<AQRPD> stationList = DataQuery.GetAQRPSDFromHistory(beginTime, endTime);
-                SqlHelper.Default.Insert(stationList.GetDataTable("AQRPSDHistory"));
+                if (GetTime())
+                {
+                    List<AQRPD> stationList = DataQuery.GetAQRPSDFromHistory(beginTime, endTime);
+                    SqlHelper.Default.Insert(stationList.GetDataTable("AQRPSDHistory"));
+                    textBox3.Text = string.Format("Succeed,{0}", DateTime.Now);
+                }
             }
             catch (Exception ex)
             {
+                textBox3.Text = string.Format("failed,{0}", DateTime.Now);
                 logger.Error("Insert AQRPSDHistory failed.", ex);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
             try
             {
-                List<AQRPD> cityList = DataQuery.GetAQRPCDFromHistory(beginTime, endTime);
-                SqlHelper.Default.Insert(cityList.GetDataTable("AQRPCDHistory"));
+                if (GetTime())
+                {
+                    List<AQRPD> list = DataQuery.GetAQRPCDFromHistory(beginTime, endTime);
+                    SqlHelper.Default.Insert(list.GetDataTable("AQRPCDHistory"));
+                    textBox3.Text = string.Format("Succeed,{0}", DateTime.Now);
+                }
             }
             catch (Exception ex)
             {
-                logger.Error("Insert AQRPSDHistory failed.", ex);
+                textBox3.Text = string.Format("failed,{0}", DateTime.Now);
+                logger.Error("Insert AQRPCDHistory failed.", ex);
             }
-            endTime = DateTime.Today.AddDays(-1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
             try
             {
-                List<AQDPD> stationList = DataQuery.GetAQDPSDFromHistory(beginTime, endTime);
-                SqlHelper.Default.Insert(stationList.GetDataTable("AQDPSDHistory"));
+                if (GetTime())
+                {
+                    List<AQDPD> stationList = DataQuery.GetAQDPSDFromHistory(beginTime, endTime);
+                    SqlHelper.Default.Insert(stationList.GetDataTable("AQDPSDHistory"));
+                    textBox3.Text = string.Format("Succeed,{0}", DateTime.Now);
+                }
             }
             catch (Exception ex)
             {
+                textBox3.Text = string.Format("failed,{0}", DateTime.Now);
                 logger.Error("Insert AQDPSDHistory failed.", ex);
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
             try
             {
-                List<AQDPD> cityList = DataQuery.GetAQDPCDFromHistory(beginTime, endTime);
-                SqlHelper.Default.Insert(cityList.GetDataTable("AQDPCDHistory"));
+                if (GetTime())
+                {
+                    List<AQDPD> stationList = DataQuery.GetAQDPCDFromHistory(beginTime, endTime);
+                    SqlHelper.Default.Insert(stationList.GetDataTable("AQDPCDHistory"));
+                    textBox3.Text = string.Format("Succeed,{0}", DateTime.Now);
+                }
             }
             catch (Exception ex)
             {
+                textBox3.Text = string.Format("failed,{0}", DateTime.Now);
                 logger.Error("Insert AQDPCDHistory failed.", ex);
             }
-            textBox1.Text = "Done";
         }
     }
 }
